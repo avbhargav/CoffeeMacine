02f56befa28165352523d87dcdc2b8b959dceeee
avbhargav <avbhargav.it@gmail.com>
2025-05-25 23:06:49 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/machine/CoffeeMachine.java

--- src/machine/CoffeeMachine.java
+++ src/machine/CoffeeMachine.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
     public boolean isAlive = true;
+
+    private int userCoffeeChoice;
     public static final Scanner scan = new Scanner(System.in);
     private final MoneyManager moneyManager = new MoneyManager();
     private final ResourceManager resourceManager = new ResourceManager(400, 540, 120, 9);
-    private Coffee coffeeType;
 
 
     public static void main(String[] args) {
@@ -25,72 +26,76 @@ public class CoffeeMachine {
 
     public void actionController(String action) {
         switch (action) {
-            case "buy":
-                int coffeeType = this.displayCoffeeOptionsAndCollectOrder();
-                String msg = this.collectMoneyAndServeCoffee(coffeeType);
-                System.out.println(msg);
-                break;
-            case "fill":
-                resourceManager.collectAndFillResources();
-                break;
-            case "take":
-                moneyManager.giveOutMoney();
-                break;
-            case "remaining":
+            case "buy" -> {
+                this.takeOrder();
+                this.collectMoneyAndServeCoffee(this.userCoffeeChoice);
+            }
+            case "fill" -> resourceManager.collectAndFillResources();
+            case "take" -> moneyManager.giveOutMoney();
+            case "remaining" -> {
                 resourceManager.displayResources();
                 moneyManager.displayMoneyEarned();
-                break;
-            case "exit":
-                this.stopCoffeeMachine();
-                break;
-            default:
-                System.out.println("Invalid Service Entered");
+            }
+            case "exit" -> this.stopCoffeeMachine();
+            default -> System.out.println("Invalid Service Entered");
         }
     }
 
-    private String collectMoneyAndServeCoffee(int userChoice) {
-
-        String msg = "";
-
-        switch (userChoice) {
-            case 1:
-                coffeeType = new Coffee("espresso",250, 0, 16, 1);
-                this.checkResourcesAndMakeCoffee(coffeeType);
-                break;
-            case 2:
-                coffeeType = new Coffee("latte",350, 75, 20, 7);
-                this.checkResourcesAndMakeCoffee(coffeeType);
-                break;
-            case 3:
-                coffeeType = new Coffee("cappuccino",200, 100, 12, 6);
-                this.checkResourcesAndMakeCoffee(coffeeType);
-                break;
-            default:
-                return "Please choose from Available Options";
+    private void takeOrder() {
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
+        String input = scan.next();
+        // Try to parse the input as an integer
+        try {
+            this.userCoffeeChoice = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            if(input.equals("back")){
+                this.askForAction();
+            }
         }
-        return "Here is your " + coffeeType.getCOFFEE_NAME() + ", Have a Great Day!";
+    }
+
+    private void collectMoneyAndServeCoffee(int userChoice) {
+
+        Coffee coffeeType;
+        switch (userChoice) {
+            case 1 -> {
+                coffeeType = new Coffee("espresso", 250, 0, 16, 1, 4);
+                this.checkResourcesAndMakeCoffee(coffeeType);
+            }
+            case 2 -> {
+                coffeeType = new Coffee("latte", 350, 75, 20, 1, 7);
+                this.checkResourcesAndMakeCoffee(coffeeType);
+            }
+            case 3 -> {
+                coffeeType = new Coffee("cappuccino", 200, 100, 12, 1, 6);
+                this.checkResourcesAndMakeCoffee(coffeeType);
+            }
+            default -> {
+                System.out.println("Please choose from Available Options");
+            }
+        }
+
     }
 
     public void checkResourcesAndMakeCoffee(Coffee coffeeType){
-        if(resourceManager.isHasSufficientResources()){
+
+        resourceManager.findScarceResource(coffeeType);
+
+        if(resourceManager.hasSufficientResources()){
             System.out.println("I have enough resources, making you a coffee!");
             moneyManager.collectMoney(coffeeType.getCOST_OF_COFFEE());
             makeCoffee(coffeeType);
+            System.out.println("Here is your " + coffeeType.getCOFFEE_NAME() + ", Have a Great Day! \n");
         } else {
-            System.out.println("Sorry, not enough "+resourceManager.limitedResource(coffeeType)+"!");
+            System.out.println("Sorry, not enough "+ resourceManager.getScarceItem() +"! \n");
         }
     }
 
-    private int displayCoffeeOptionsAndCollectOrder() {
-        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
-        return scan.nextInt();
-    }
-
     public void makeCoffee(Coffee coffee) {
-        this.availWater -= water;
-        this.availMilk -= milk;
-        this.availCoffeeBeans -= coffeeBeans;
-        this.availDisposableCups -= CoffeeMachine.DISPOSABLE_CUPS;
+        resourceManager.setAvailWater(resourceManager.getAvailWater() - coffee.getWATER_NEEDED());
+        resourceManager.setAvailMilk(resourceManager.getAvailMilk() - coffee.getMILK_NEEDED());
+        resourceManager.setAvailCoffeeBeans(resourceManager.getAvailCoffeeBeans() - coffee.getCOFFEE_BEANS_NEEDED());
+        resourceManager.setAvailDisposableCups(resourceManager.getAvailDisposableCups() - coffee.getDISPOSABLE_CUPS());
     }
 
     private void stopCoffeeMachine() {
@@ -100,15 +105,10 @@ public class CoffeeMachine {
 
 
 class MoneyManager {
-
-    public static final int COST_OF_ESPRESSO = 4;
-    public static final int COST_OF_LATTE = 7;
-    public static final int COST_OF_CAPPUCCINO = 6;
-
     public int moneyEarned = 550;
 
     public void displayMoneyEarned() {
-        System.out.println(this.moneyEarned +"$ of money\"");
+        System.out.println(this.moneyEarned +"$ of money\n");
     }
 
     public void giveOutMoney() {
@@ -128,12 +128,45 @@ class ResourceManager {
     private int availCoffeeBeans;
     private int availDisposableCups;
 
-    public boolean isHasSufficientResources() {
-        return hasSufficientResources;
+    public int getAvailWater() {
+        return availWater;
     }
 
+    public void setAvailWater(int availWater) {
+        this.availWater = availWater;
+    }
+
+    public int getAvailMilk() {
+        return availMilk;
+    }
+
+    public void setAvailMilk(int availMilk) {
+        this.availMilk = availMilk;
+    }
+
+    public int getAvailCoffeeBeans() {
+        return availCoffeeBeans;
+    }
+
+    public void setAvailCoffeeBeans(int availCoffeeBeans) {
+        this.availCoffeeBeans = availCoffeeBeans;
+    }
+
+    public int getAvailDisposableCups() {
+        return availDisposableCups;
+    }
+
+    public void setAvailDisposableCups(int availDisposableCups) {
+        this.availDisposableCups = availDisposableCups;
+    }
+
+    public String getScarceItem() {
+        return scarceItem;
+    }
+
+    private String scarceItem;
     private boolean hasSufficientResources;
-    private Scanner scan = new Scanner(System.in);
+    private final Scanner scan = new Scanner(System.in);
 
     public ResourceManager(int availMilk, int availWater, int availCoffeeBeans, int availDisposableCups) {
         this.availMilk = availWater;
@@ -142,29 +175,27 @@ class ResourceManager {
         this.availDisposableCups = availDisposableCups;
     }
 
-    public String limitedResource(Coffee coffeeType){
-        String lowStockItem = "";
+    public void findScarceResource(Coffee coffeeType){
+
         if(this.availWater < coffeeType.getWATER_NEEDED()){
-            lowStockItem = "water";
+            this.scarceItem = "water";
             this.hasSufficientResources = false;
         } else if(this.availMilk < coffeeType.getMILK_NEEDED()){
-            lowStockItem = "milk";
+            this.scarceItem = "milk";
             this.hasSufficientResources = false;
         } else if(this.availCoffeeBeans < coffeeType.getCOFFEE_BEANS_NEEDED()){
-            lowStockItem = "coffee beans";
+            this.scarceItem = "coffeeBeans";
             this.hasSufficientResources = false;
         } else if(this.availDisposableCups < coffeeType.getDISPOSABLE_CUPS()){
-            lowStockItem = "disposable cups";
+            this.scarceItem = "disposable cups";
             this.hasSufficientResources = false;
         } else {
             this.hasSufficientResources = true;
         }
-        return lowStockItem;
     }
 
-
-    public void getResources(String coffeeType){
-
+    public boolean hasSufficientResources() {
+        return hasSufficientResources;
     }
 
     public void collectAndFillResources() {
@@ -193,14 +224,15 @@ class Coffee {
     private final int WATER_NEEDED;
     private final int MILK_NEEDED;
     private final int COFFEE_BEANS_NEEDED;
-    private final int DISPOSABLE_CUPS = 1;
+    private final int DISPOSABLE_CUPS;
     private final int COST_OF_COFFEE;
 
-    public Coffee(String coffeeName, int water, int milk, int coffeeBeans, int costOfCoffee){
+    public Coffee(String coffeeName, int water, int milk, int coffeeBeans, int disposableCups, int costOfCoffee){
         this.COFFEE_NAME = coffeeName;
         this.WATER_NEEDED = water;
         this.MILK_NEEDED = milk;
         this.COFFEE_BEANS_NEEDED = coffeeBeans;
+        DISPOSABLE_CUPS = disposableCups;
         this.COST_OF_COFFEE = costOfCoffee;
     }
 
